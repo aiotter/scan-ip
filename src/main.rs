a5c6f0b290986c6a00609e3d6e1160b4bc31cf45
aiotter <git@aiotter.com>
2023-05-08 14:47:42 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -9,55 +9,37 @@ fn main() {
     let interface = get_interface();
     let (mut _tx, mut rx) = match datalink::channel(&interface, Default::default()) {
         Ok(datalink::Channel::Ethernet(tx, rx)) => (tx, rx),
-        Ok(..) => panic!("Unsupported channel type"),
-        Err(..) => panic!("Cannot create channel for {}", interface.name),
+        Ok(_) => panic!("Unsupported channel type"),
+        Err(_) => panic!("Cannot create channel for {}", interface.name),
     };
 
     while let Ok(frame) = rx.next() {
-        let packet = match ethernet::EthernetPacket::new(frame) {
-            Some(packet) => packet,
-            None => continue,
-        };
-        let packet = match packet.get_ethertype() {
-            ethernet::EtherTypes::Arp => packet,
+        let ethernet_packet = match ethernet::EthernetPacket::new(frame) {
+            Some(packet) if packet.get_ethertype() == ethernet::EtherTypes::Arp => packet,
             _ => continue,
         };
-        let packet = ArpPacket::new(packet.payload()).unwrap();
-        let arp = ArpPacket::from_packet(&packet);
-        match &arp.operation {
-            &ArpOperations::Reply => {
-                let sender_mac = &arp.sender_hw_addr;
-                let sender_ip = &arp.sender_proto_addr;
-                let target_mac = &arp.target_hw_addr;
-                let target_ip = &arp.target_proto_addr;
-                println!(
-                    "{}({}) -> {}({})",
-                    sender_ip, sender_mac, target_ip, target_mac
-                );
-            }
-            &ArpOperations::Request => continue,
-            _ => unreachable!("Unsupported ARP operation code"),
-        }
+        let arp_packet = ArpPacket::new(ethernet_packet.payload()).unwrap();
+        let arp_data = ArpPacket::from_packet(&arp_packet);
+        match arp_data.operation {
+            ArpOperations::Reply => (),
+            ArpOperations::Request => continue,
+            _ => panic!("Unsupported ARP operation code"),
+        };
+
+        let sender_mac = &arp_data.sender_hw_addr;
+        let sender_ip = &arp_data.sender_proto_addr;
+        let target_mac = &arp_data.target_hw_addr;
+        let target_ip = &arp_data.target_proto_addr;
+        println!("{sender_ip}({sender_mac}) -> {target_ip}({target_mac})",);
     }
 }
 
 fn get_interface() -> datalink::NetworkInterface {
     datalink::interfaces()
         .into_iter()
-        .find(|interface| {
-            if interface.mac.is_none() {
-                return false;
-            }
-
-            if interface.ips.is_empty() || !interface.is_up() || interface.is_loopback() {
-                return false;
-            }
-
-            if interface.ips.iter().find(|ip| ip.is_ipv4()).is_some() {
-                return true;
-            };
-
-            false
-        })
+        .filter(|interface| !interface.mac.is_none())
+        .filter(|interface| interface.is_up() && !interface.is_loopback())
+        .filter(|interface| interface.ips.iter().any(|ip| ip.is_ipv4()))
+        .next()
         .unwrap()
 }
